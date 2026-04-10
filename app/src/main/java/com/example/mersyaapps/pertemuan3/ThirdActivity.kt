7797f530b8638f56c261c7a9dfc6751cdeae8749
lab156-31 <mersya24si@mahassiwa.pcr.ac.id>
2026-04-10 08:24:14 +0700

    Pertemuan 3 : penerapan wiewBinding

---
+++ app/src/main/java/com/example/mersyaapps/pertemuan3/ThirdActivity.kt
@@ -0,0 +1,40 @@
+package com.example.mersyaapps.pertemuan3
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mersyaapps.R
+import com.example.mersyaapps.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityThirdBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnKirim.setOnClickListener {
+            val nomor = binding.inputNoTujuan.text
+            Log.e("Klik btnSubmit","Tombol berhasil di tekan. Isi dari inputNama = ")
+
+            Toast.makeText(this, "Pesan berhasil dikirim ke : $nomor", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+}
