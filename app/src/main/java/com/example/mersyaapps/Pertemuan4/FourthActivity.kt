5baba6f557cbba451315cfd557c9557315eb44ae
lab156-31 <mersya24si@mahassiwa.pcr.ac.id>
2026-04-17 08:00:02 +0700

    Pertemuan 4 : warming up

--- app/src/main/java/com/example/mersyaapps/Pertemuan4/FourthActivity.kt
+++ app/src/main/java/com/example/mersyaapps/Pertemuan4/FourthActivity.kt
@@ -1,37 +1,24 @@
-package com.example.mersyaapps
+package com.example.mersyaapps.Pertemuan4
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.mersyaapps.Pertemuan4.FourthActivity
+import com.example.mersyaapps.R
 import com.example.mersyaapps.databinding.ActivityFourthBinding
-import com.example.mersyaapps.databinding.ActivityMainBinding
+import com.example.mersyaapps.databinding.ActivityThirdBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
+class FourthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_fourth)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        binding.button2.setOnClickListener {
-            val intent = Intent(this, FourthActivity::class.java)
-            startActivity(intent)
-
         }
     }
 }
