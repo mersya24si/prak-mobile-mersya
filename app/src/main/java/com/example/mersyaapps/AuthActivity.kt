eb7189611b79122d4fa21deea4657978956e1d20
mersya24si <mersya24si@mahasiswa.pcr.ac.id>
2026-05-07 21:49:23 +0700

    Pertemuan 6 AuthActivity dan SharedPrefences

---
+++ app/src/main/java/com/example/mersyaapps/AuthActivity.kt
@@ -0,0 +1,61 @@
+package com.example.mersyaapps
+
+import android.app.AlertDialog
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mersyaapps.databinding.ActivityAuthBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+class AuthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAuthBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+        val sharedPref = getSharedPreferences("user_pref", MODE_PRIVATE)
+
+        //Kondisi jika isLogin bernilai true
+//        val isLogin = sharedPref.getBoolean("isLogin", false)
+//        if (isLogin) {
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()//kill auth activity
+//        }
+        binding.btnLogin.setOnClickListener {
+            val username = binding.username.text.toString()
+            val password = binding.password.text.toString()
+
+            if (username == password && username.isNotEmpty() && password.isNotEmpty()) {
+                sharedPref.edit {
+                    putBoolean("isLogin", true)
+                    putString("username", username)
+                }
+                Toast.makeText(this, " $username Login Anda Berhasil !", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("username", binding.username.text.toString()) // Kuncinya adalah "username"
+                startActivity(intent)
+            } else {5
+                MaterialAlertDialogBuilder(this)
+                    .setTitle("Coba Lagi")
+                    .setMessage("Silahkan Coba lagi")
+                    .setPositiveButton("Ya") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/mersyaapps/MainActivity.kt
+++ app/src/main/java/com/example/mersyaapps/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.mersyaapps
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -11,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.mersyaapps.Pertemuan4.FourthActivity
 import com.example.mersyaapps.databinding.ActivityFourthBinding
 import com.example.mersyaapps.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +30,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val sharedPref = getSharedPreferences("user_pref", MODE_PRIVATE)
 
         binding.button2.setOnClickListener {
             val intent = Intent(this, FourthActivity::class.java)
@@ -36,6 +40,26 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("usia", 25)
 
             startActivity(intent)
+
+        }
+        binding.btnLogout.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Konfirmasi")
+                .setMessage("Apakah Anda yakin ingin melanjutkan?")
+                .setPositiveButton("Ya") { dialog, _ ->
+                    val editor = sharedPref.edit()
+                    editor.clear()
+                    editor.apply()
+
+                    dialog.dismiss()
+                    val intent = Intent(this, AuthActivity::class.java)
+                    startActivity(intent)
+                }
+                .setNegativeButton("Batal") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog","Anda memilih Tidak!")
+                }
+                .show()
 
         }
     }
