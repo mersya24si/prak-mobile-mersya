22b51342e0fecab4225b22060730f727617090b3
lab156-31 <mersya24si@mahassiwa.pcr.ac.id>
2026-06-12 09:59:41 +0700

    Pertemuan 12: Sesi 2

--- app/src/main/java/com/example/mersyaapps/Note/NoteFragment.kt
+++ app/src/main/java/com/example/mersyaapps/Note/NoteFragment.kt
@@ -1,17 +1,29 @@
 package com.example.mersyaapps.Note
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mersyaapps.data.AppDatabase
+import com.example.mersyaapps.data.entity.NoteEntity
 import com.example.mersyaapps.databinding.FragmentNoteBinding
+import kotlinx.coroutines.launch
 
 class NoteFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var _binding: FragmentNoteBinding? = null
     private val binding get() = _binding!!
+
+    /** Variabel database & Adapter **/
+    private lateinit var adapter: NoteAdapter
+    private lateinit var db: AppDatabase
+    private val notes = mutableListOf<NoteEntity>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,9 +33,46 @@ class NoteFragment : Fragment() {
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
         (requireActivity() as AppCompatActivity).supportActionBar?.apply {
             title = "Note"
         }
+        binding.fabAddNote.setOnClickListener {
+            startActivity(Intent(requireContext(), NoteFormActivity::class.java))
+        }
+
+        /** Inisialisasi AppDatabase & Adapter **/
+        db = AppDatabase.getInstance(requireContext())
+        adapter = NoteAdapter(notes, this)
+
+        binding.rvNotes.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvNotes.adapter = adapter
+
+        /** Tambah ini sebagai garis pemisah **/
+        val dividerItemDecoration = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        binding.rvNotes.addItemDecoration(dividerItemDecoration)
+
+        fetchNotes()
+    }
+
+    private fun fetchNotes() {
+        lifecycleScope.launch {
+            val data = db.noteDao().getAll() //pemanggilan query
+            notes.clear()
+            notes.addAll(data)
+            adapter.notifyDataSetChanged()
+        }
+    }
+    fun deleteNote(note: NoteEntity) {
+        lifecycleScope.launch {
+            db.noteDao().delete(note) //Hapus Note
+            fetchNotes()              //Fetch lagi data notes terbaru
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        fetchNotes()
     }
 }
--- app/src/main/java/com/example/mersyaapps/data/dao/NoteDao.kt
+++ app/src/main/java/com/example/mersyaapps/data/dao/NoteDao.kt
@@ -1,6 +1,7 @@
 package com.example.mersyaapps.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
@@ -17,4 +18,6 @@ interface NoteDao {
     @Update
     suspend fun update (note: NoteEntity)
 
+    @Delete
+    suspend fun delete(note: NoteEntity)
 }
